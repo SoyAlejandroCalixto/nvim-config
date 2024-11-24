a6d51c5ac19fdf8c9687f4c92c470fabdf46010e
soyalejandrocalixto <soyalejandrocalixto@gmail.com>
2024-11-24 05:57:09 +0000

    added bufferline

--- init.lua
+++ init.lua
@@ -13,6 +13,7 @@ vim.keymap.set('n', '<C-h>', '<C-w>h')
 vim.keymap.set('n', '<C-l>', '<C-w>l')
 vim.keymap.set('n', '<C-j>', '<C-w>j')
 vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('t', '<C-x>', [[<C-\><C-n>]])
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -106,5 +106,15 @@ return {
     },
     {
         "nvim-tree/nvim-web-devicons"
+    },
+    {
+        'akinsho/bufferline.nvim',
+        version = "*",
+        dependencies = 'nvim-tree/nvim-web-devicons',
+        config = function()
+            require("bufferline").setup{}
+            vim.keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>")
+            vim.keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>")
+        end
     }
 }

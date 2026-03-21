b9e3a879a3267da8b860281e025fe01e4444eae2
soyalejandrocalixto <soyalejandrocalixto@gmail.com>
2026-03-21 12:33:34 +0000

    Upgraded filter config

--- lua/config/noice.lua
+++ lua/config/noice.lua
@@ -1,16 +1,34 @@
 require('noice').setup({
-    routes = {
-        { filter = { event = 'msg_show', find = 'línea más' }, opts = { skip = true }, },
-        { filter = { event = 'msg_show', find = 'líneas más' }, opts = { skip = true }, },
-        { filter = { event = 'msg_show', find = 'línea menos' }, opts = { skip = true }, },
-        { filter = { event = 'msg_show', find = 'líneas menos' }, opts = { skip = true }, },
-        { filter = { event = 'msg_show', find = 'fewer lines' }, opts = { skip = true }, },
-        { filter = { event = 'msg_show', find = 'cambio;' }, opts = { skip = true }, },
-        { filter = { event = 'msg_show', find = 'cambios;' }, opts = { skip = true }, },
-        { filter = { event = 'msg_show', find = 'Este es el cambio más' }, opts = { skip = true }, },
-        { filter = { event = 'msg_show', find = 'lines yanked' }, opts = { skip = true }, },
-        { filter = { event = 'msg_show', find = 'escritos' }, opts = { skip = true }, },
-        { filter = { event = 'msg_show', find = 'lines indented' }, opts = { skip = true }, },
-        { filter = { error = true, find = 'server cancelled the request' }, opts = { skip = true }, },
+  routes = {
+    {
+      filter = {
+        event = "msg_show",
+        any = {
+          { find = "more lines" },
+          { find = "fewer lines" },
+          { find = "cambio[s]?;" },
+          { find = "Este es el cambio más" },
+          { find = "line[s]? yanked" },
+          { find = "escritos" },
+          { find = "lines indented" },
+        },
+      },
+      opts = { skip = true },
     },
+    {
+      filter = {
+        event = "lsp",
+        kind = "progress",
+        find = "jdtls"
+      },
+      opts = { skip = true }
+    },
+    {
+      filter = {
+        error = true,
+        find = "server cancelled the request"
+      },
+      opts = { skip = true }
+    }
+  },
 })
